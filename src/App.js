19c21ce3260744bed826eb738c9d1245b927aba7
inkxk_ <nokink@hotmail.com>
2021-12-01 16:55:36 +0700

    finish cards

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/page/Home';
+import Services from './components/page/Services';
+import Products from './components/page/Products';
+import SignUp from './components/page/SignUp';
 
 
 function App() {
 	return (
 		<>
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" />
-			</Routes>
-		</Router>
-		
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/services" element={<Services />} />
+					<Route path="/products" element={<Products />} />
+					<Route path="/sign-up" element={<SignUp />} />
+				</Routes>
+			</Router>
     	</>
   	);
 }
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
 import { Button } from './Button';
+import '../App.css';
+import './HeroSection.css';
+import video from '../assets/videos/video-2.mp4';
 
 function HeroSection() {
     return (
         <div className="hero-container">
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
+            {/* <video src={video} autoPlay loop muted /> */}
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
             <div className="hero-btns">
@@ -14,6 +17,13 @@ function HeroSection() {
                     buttonSize="btn--large"
                 >
                     GET STARTED
+                </Button>
+                <Button
+                    className="btns"
+                    buttonStyle="btn--primary"
+                    buttonSize="btn--large"
+                >
+                    WATCH TRAILER <i className="far fa-play-circle" />
                 </Button>
             </div>
         </div>
